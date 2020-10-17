130176e894ac182b21a1251b20b552bd6bab5232
afkingshuk <afkingshuk@gmail.com>
2020-10-18 04:31:17 +0600

    backend tweak

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const auth = require('../middleware/auth');
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -14,10 +14,10 @@ const User = require('../models/User');
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
+    return res.json(user);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    return res.status(500).send('Server Error');
   }
 });
 
@@ -33,22 +33,22 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({errors: errors.array()});
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     try {
-      let user = await User.findOne({email});
+      let user = await User.findOne({ email });
 
       if (!user) {
-        return res.status(400).json({msg: 'Invalid Credentials'});
+        return res.status(400).json({ msg: 'Invalid Credentials' });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({msg: 'Invalid Credentials'});
+        return res.status(400).json({ msg: 'Invalid Credentials' });
       }
 
       const payload = {
@@ -65,12 +65,12 @@ router.post(
         },
         (err, token) => {
           if (err) throw err;
-          res.json({token});
+          return res.json({ token });
         },
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      return res.status(500).send('Server Error');
     }
   },
 );
